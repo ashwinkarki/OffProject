1b01c53ccc998d35ddd0966190493af9ad611e78
ashwinkarki <ashwin.karki123@yahoo.com>
2019-03-04 06:38:34 +0545

    abc

---
+++ OfficeProject/src/main/java/com/ashwin/officeproject/OfficeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ashwin.officeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeProjectApplication.class, args);
+	}
+
+}
